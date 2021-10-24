a7408e8162e595bb9e7e0a76c6c512f4ff8a9517
956622329 <956622329@qq.com>
2021-10-24 10:32:15 +0800

    FastClick

--- src/main.js
+++ src/main.js
@@ -4,14 +4,21 @@ import router from './router'
 import store from './store'
 
 import FastClick from 'fastclick'
+import VueLazyload from 'vue-lazyload'
 
 import toast from 'components/common/toast'
 
 Vue.config.productionTip = false
 //安装toast插件
 Vue.use(toast)
+
 //解决移动端300毫秒
 FastClick.attach(document.body)
+
+//使用懒加载插件
+Vue.use(VueLazyload, {
+  loading:require('./assets/img/common/placeholder.png')
+})
 
 new Vue({
   render: h => h(App),
